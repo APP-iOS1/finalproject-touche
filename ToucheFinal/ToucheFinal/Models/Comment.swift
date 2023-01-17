050c8030ced7413a9a58ed2cf2c35119278dabbf
yooj <114331071+yooj1202@users.noreply.github.com>
2023-01-17 13:45:46 +0900

    Add files via upload

---
+++ ToucheFinal/ToucheFinal/Models/Comment.swift
@@ -0,0 +1,14 @@
+//
+//  Comment.swift
+//  ToucheFinal
+//
+//  Created by Yooj on 2023/01/17.
+//
+
+import Foundation
+
+struct Comment {
+    var commentId : String
+    var commentTime : String
+    var contents : String
+}
---
+++ ToucheFinal/ToucheFinal/Store/UserInfoStore.swift
@@ -0,0 +1,8 @@
+//
+//  UserInfoStore.swift
+//  ToucheFinal
+//
+//  Created by Yooj on 2023/01/17.
+//
+
+import Foundation
