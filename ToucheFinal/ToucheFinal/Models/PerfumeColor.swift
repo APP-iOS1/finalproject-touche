de58c251594747fefd1e8b5423382500f52d88db
서광현 (seo-kh) <gospelseokh@gmail.com>
2023-01-30 12:14:33 +0900

    [Feat] : 향수와 연관된 색 정의 1) 데이터 모델, 2) Color extension 코드 파일 생성

---
+++ ToucheFinal/ToucheFinal/Models/PerfumeColor.swift
@@ -0,0 +1,37 @@
+//
+//  PerfumeColor.swift
+//  ToucheFinal
+//
+//  Created by james seo on 2023/01/30.
+//
+
+import SwiftUI
+
+struct PerfumeColor: Identifiable {
+    var id = UUID().uuidString
+    var name: String
+    var hexValue: String
+    var color: Color {
+        Color(hex: hexValue) ?? Color.clear
+    }
+    
+    static var types: [PerfumeColor] = [
+        PerfumeColor(name: "Fruity Florals", hexValue: "EC3C3C"),
+        PerfumeColor(name: "Warm & Sweet Gourmands", hexValue: "A11818"),
+        PerfumeColor(name: "Warm Florals", hexValue: "E26262"),
+        PerfumeColor(name: "Warm Woods", hexValue: "895151"),
+        PerfumeColor(name: "Fresh Florals", hexValue: "FF9680"),
+        PerfumeColor(name: "Fresh Citrus & Fruits", hexValue: "FCE182"),
+        PerfumeColor(name: "Classic Florals", hexValue: "EC96E3"),
+        PerfumeColor(name: "Woody Spices", hexValue: "513737"),
+        PerfumeColor(name: "Cool Spices", hexValue: "326D98"),
+        PerfumeColor(name: "Classic Woods", hexValue: "595252"),
+        PerfumeColor(name: "Citrus & Woods", hexValue: "B2A795"),
+        PerfumeColor(name: "Fresh Solar", hexValue: "F0BC00"),
+        PerfumeColor(name: "Warm & Sheer", hexValue: "B8B0B0"),
+        PerfumeColor(name: "Powdery Florals", hexValue: "D933C7"),
+        PerfumeColor(name: "Fresh Aquatics", hexValue: "2C5DDA"),
+        PerfumeColor(name: "Earthy Greens & Herbs", hexValue: "4B9A4E"),
+    ]
+}
+
