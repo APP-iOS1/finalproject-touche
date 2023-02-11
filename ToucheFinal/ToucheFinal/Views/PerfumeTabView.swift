5cb14bed2a34e34e87cf6476398a1910c1d0a206
KTS224 <ts4683@gmail.com>
2023-02-11 19:48:50 +0900

    [Fix] : perfumeTabView 향수 이름 데이터 불러오기 버그 수정
    - 데이터 불러오는 시간 고려해서 2초 시간 지연 추가

--- ToucheFinal/ToucheFinal/Views/PerfumeTabView.swift
+++ ToucheFinal/ToucheFinal/Views/PerfumeTabView.swift
@@ -69,25 +69,27 @@ struct PerfumeTabView: View {
                 }
                 .onAppear {
                     print(selectedColors)
-                    print("perfumesDB.count : \(perfumesDB.count)")
-                    print("USERDEFAULTS . CountOfDB : \(NameOfAllPerfumes.count)")
-                    /// 로컬 향수 이름들의 갯수와 BD 향수 이름 개수가 다를 경우 향수 이름들 다시 패치한다
-                    if NameOfAllPerfumes.count != perfumesDB.count {
-                        print("향수 개수 다름!! 향수 이름 패치 시작")
-                        var searchResults: [String] {
-                            let perfumeNames = perfumesDB.filter { perfume in
-                                perfume.displayName != ""
-                            }
-                                .map { $0.displayName}
-                            return perfumeNames
-                        }
-                        UserDefaults.standard.set(searchResults, forKey: "NameOfAllPerfumes")
-                        
+                    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 2) {
                         print("perfumesDB.count : \(perfumesDB.count)")
                         print("USERDEFAULTS . CountOfDB : \(NameOfAllPerfumes.count)")
-
-                    } else {
-                        print("향수 이름 개수 데이터 변동 없음.")
+                        
+                        /// 로컬 향수 이름들의 갯수와 BD 향수 이름 개수가 다를 경우 향수 이름들 다시 패치한다
+                        if NameOfAllPerfumes.count != perfumesDB.count {
+                            print("향수 개수 다름!! 향수 이름 패치 시작")
+                            var searchResults: [String] {
+                                let perfumeNames = perfumesDB.filter { perfume in
+                                    perfume.displayName != ""
+                                }
+                                    .map { $0.displayName}
+                                return perfumeNames
+                            }
+                            
+                            UserDefaults.standard.set(searchResults, forKey: "NameOfAllPerfumes")
+                            print("perfumesDB.count : \(perfumesDB.count)")
+                            print("USERDEFAULTS . CountOfDB : \(NameOfAllPerfumes.count)")
+                        } else {
+                            print("향수 이름 개수 데이터 변동 없음.")
+                        }
                     }
                 }
             }
